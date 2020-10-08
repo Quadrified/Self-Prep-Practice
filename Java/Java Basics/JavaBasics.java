be12ce7ccdca9a82b611c4d62588a5d117cb1119
DSMac <mohd.omerquadri@dsmartsys.in>
2020-10-08 23:27:36 +0530

    Add Java prep

---
+++ Java/Java Basics/JavaBasics.java
@@ -0,0 +1,13 @@
+/**
+ * JavaBasics
+ */
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        String arr[] = { "Hello", "World" };
+
+        for (String str : arr) {
+            System.out.println(str);
+        }
+    }
+}
