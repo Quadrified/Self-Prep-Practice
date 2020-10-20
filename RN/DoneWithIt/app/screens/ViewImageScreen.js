02b63912c699b92d04ed7a8a1c50a34afffcdf24
DSMac <mohd.omerquadri@dsmartsys.in>
2020-10-21 01:24:57 +0530

    Styling: Flex, Custom icons, Layouts

--- RN/DoneWithIt/app/screens/ViewImageScreen.js
+++ RN/DoneWithIt/app/screens/ViewImageScreen.js
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
+
+import colors from '../configs/colors';
 
 class ViewImageScreen extends Component {
   render() {
-    return <View></View>;
+    return (
+      <View style={styles.container}>
+        <View style={styles.closeIcon}></View>
+        <View style={styles.deleteIcon}></View>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../assets/chair.jpg')}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.black,
+  },
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.primary,
+    position: 'absolute',
+    top: 10,
+    left: 15,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.secondary,
+    position: 'absolute',
+    top: 10,
+    right: 15,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default ViewImageScreen;
