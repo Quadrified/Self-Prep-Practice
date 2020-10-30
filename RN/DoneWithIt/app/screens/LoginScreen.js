68facfbfc9805bb562327f329a81c7f86b9dd01c
DSMac <mohd.omerquadri@dsmartsys.in>
2020-10-31 02:37:21 +0530

    Add Yup and Formik

--- RN/DoneWithIt/app/screens/LoginScreen.js
+++ RN/DoneWithIt/app/screens/LoginScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import {StyleSheet, Image} from 'react-native';
+import {Formik} from 'formik';
+import {Image, StyleSheet} from 'react-native';
 import AppButton from '../components/AppButton';
 import AppTextInput from '../components/AppTextInput';
 import Screen from '../components/Screen';
 import colors from '../configs/colors';
-import {Formik} from 'formik';
+import * as Yup from 'yup';
+import AppText from '../components/AppText';
+import AppErrorMessage from '../components/AppErrorMessage';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label('Email'),
+  password: Yup.string().required().min(4).max(5).label('Password'),
+});
 
 function LoginScreen(props) {
   return (
@@ -13,8 +21,9 @@ function LoginScreen(props) {
 
       <Formik
         initialValues={{email: '', password: ''}}
-        onSubmit={(values) => console.log(values)}>
-        {({handleChange, handleSubmit}) => (
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}>
+        {({handleChange, handleSubmit, errors, setFieldTouched, touched}) => (
           <>
             <AppTextInput
               icon="email"
@@ -23,7 +32,9 @@ function LoginScreen(props) {
               autoCorrect={false}
               keyboardType="email-address"
               onChangeText={handleChange('email')}
+              onBlur={() => setFieldTouched('email')}
             />
+            <AppErrorMessage error={errors.email} visible={touched.email} />
 
             <AppTextInput
               icon="form-textbox-password"
@@ -32,6 +43,11 @@ function LoginScreen(props) {
               autoCorrect={false}
               secureTextEntry
               onChangeText={handleChange('password')}
+              onBlur={() => setFieldTouched('password')}
+            />
+            <AppErrorMessage
+              error={errors.password}
+              visible={touched.password}
             />
 
             <AppButton
