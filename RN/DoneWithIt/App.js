02ecbc972ec32fac20b422cb38a6e980e9df955e
DSMac <mohd.omerquadri@dsmartsys.in>
2020-11-10 21:54:45 +0530

    Imports cleanup

--- RN/DoneWithIt/App.js
+++ RN/DoneWithIt/App.js
@@ -8,25 +8,7 @@ import {
   Switch,
 } from 'react-native';
 import * as ImagePicker from 'react-native-image-crop-picker';
-
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
 import Screen from './app/components/Screen';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
-import Icon from './app/components/Icon';
-import ListingItem from './app/components/ListingItem';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import AppTextInput from './app/components/AppTextInput';
-import colors from './app/configs/colors';
-import styles from './app/components/AppButton/styles';
-import AppPicker from './app/components/AppPicker';
-import LoginScreen from './app/screens/LoginScreen';
-import ListingEditScreen from './app/screens/ListingEditScreen';
 
 const App = () => {
   return <Screen></Screen>;
