3b077d15e521813b177b35bb35b5b25b816d2a28
DSMac <mohd.omerquadri@dsmartsys.in>
2020-11-16 23:37:13 +0530

    Add useState

--- React/react-hooks-demo/src/App.js
+++ React/react-hooks-demo/src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 import TodoList from './components/TodoList';
+import UseStateHook from './Hooks/UseStateHook';
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  function handleAddTodo(e){
-    
-  }
-
   return (
-    <>
-      <TodoList todos={todos} />
-      <input type="text" name="" id="" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear completed todos</button>
-      <div>0 left to do</div>
-    </>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize: '5em',
+      }}
+    >
+      <UseStateHook />
+    </div>
   );
 }
 
