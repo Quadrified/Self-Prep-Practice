28cac17e13584d27e300bd85d0cc7d4436111175
Quadrified <omerquadri01@gmail.com>
2021-10-31 22:04:01 +0530

    Add firestore new functions

--- RN/FirebaseFeatures/app/FirebaseFirestore.js
+++ RN/FirebaseFeatures/app/FirebaseFirestore.js
@@ -1,21 +1,73 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, StatusBar } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  StatusBar,
+  Button,
+  TouchableNativeFeedback,
+  Pressable,
+} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 
 const FirebaseFirestore = props => {
   const [firestoreData, setFirestoreData] = useState(null);
   useEffect(() => {
+    /** --------------------------
+      * Getting a single doc from collection 'users'
+     --------------------------**/
+    // const subscriber = firestore()
+    //   .collection('users')
+    //   .doc('xBzttmINU5CyTgaAlJtM')
+    //   .onSnapshot(documentSnapshot => {
+    //     console.log('User data: ', documentSnapshot.data());
+    //     setFirestoreData(documentSnapshot.data());
+    //   });
+
+    /** --------------------------
+      * For getting all users from Firestore
+     --------------------------**/
     const subscriber = firestore()
       .collection('users')
-      .doc('omer')
-      .onSnapshot(documentSnapshot => {
-        console.log('User data: ', documentSnapshot.data());
-        setFirestoreData(documentSnapshot.data());
+      .onSnapshot(docs => {
+        let users = [];
+        docs.forEach(doc => {
+          users.push(doc.data());
+        });
+        console.log('User data: ', users);
+        setFirestoreData(users);
       });
 
     // Stop listening for updates when no longer required
     return () => subscriber();
   }, []);
+
+  const addRandomUser = async () => {
+    let nickName = Math.random().toString(36).substring(7);
+    let age = Math.floor(Math.random() * 50);
+    console.log('>>>name<<<', nickName, age);
+
+    // adding random user to collection as a doument
+    // await firestore()
+    //   .collection('users')
+    //   .add({ name: nickName, age, test: true })
+    //   .then(res => {
+    //     console.log('>>>res<<<', res, firestoreData);
+    //   });
+
+    // updating an existing document 'omer' under 'users' collection
+    await firestore()
+      .collection('users')
+      .doc('omer')
+      .update({
+        name: 'Omer',
+        createdAt: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(res => {
+        console.log('>>>res<<<', res, firestoreData);
+      });
+  };
+
   return (
     <>
       <StatusBar
@@ -23,8 +75,19 @@ const FirebaseFirestore = props => {
         backgroundColor={styles.container?.backgroundColor}
       />
       <View style={styles.container}>
-        <Text style={styles.text}>{firestoreData.Name}</Text>
+        {firestoreData?.map(user => (
+          <Text style={styles.text}>Name:{user.name}</Text>
+        ))}
       </View>
+
+      <Pressable
+        style={styles.buttonContainer}
+        android_ripple={{
+          color: '#ccc',
+        }}
+        onPress={addRandomUser}>
+        <Text style={styles.button}>Add Random User</Text>
+      </Pressable>
     </>
   );
 };
@@ -38,6 +101,18 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#000',
+    fontSize: 18,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#005A9C',
+    alignContent: 'center',
+  },
+  button: {
+    fontSize: 18,
   },
 });
 
